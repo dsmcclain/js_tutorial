937de300838287d847d341c81a3470184f4104a5
Dylan McClain <dylan.mcclain@gmail.com>
2018-12-20 08:13:25 -0800

    refactoring to single-purpose functions

--- palindrome.js
+++ palindrome.js
@@ -3,8 +3,36 @@ let reverse = (string) => {
 	return Array.from(string).reverse().join("");
 }
 
-// Compares a string to its reverse
-let palindrome = (string) => {
-	let filteredString = string.toLowerCase();
-	return filteredString === reverse(filteredString);
-} 
+// Defines a Phrase object.
+function Phrase(content) {
+	this.content = content;
+
+	//Returns content swithced to lower case
+	this.processor = function(string) {
+		return string.toLowerCase();
+	}
+
+	//Returns content processed for palindrome testing.
+	this.processedContent = function processedContent() {
+		return this.processor(this.content);
+	}
+
+	//Returns true if the phrase is a palindrome, false otherwise
+	this.palindrome = function palindrome() {
+		return this.processedContent() === reverse(this.processedContent());
+	}
+}
+
+
+//Defines a translatedPhrase object.
+function TranslatedPhrase(content, translation) {
+	this.content = content;
+	this.translation = translation;
+
+	//Returns translation processed for palindrome testing, overrides Phrase.processedConten
+	this.processedContent = function processedContent() {
+		return this.processor(this.translation);
+	}
+}
+
+TranslatedPhrase.prototype = new Phrase();
